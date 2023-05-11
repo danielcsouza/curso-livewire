b50ccbd9522a1ee9c8bb5dfc3d5871e91e794d7d
Daniel Carlos <daniel.carlos.souza@gmail.com>
2023-05-11 15:59:29 -0300

    autenticacao com jetstream

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,14 @@ use App\Http\Livewire\{
 };
 
 
-Route::get('/tweets', ShowTweets::class)->name('home');
+Route::get('/tweets', ShowTweets::class)->name('home')->middleware('auth');
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])
+    ->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
